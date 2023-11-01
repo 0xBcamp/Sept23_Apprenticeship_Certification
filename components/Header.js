fc957ba6694997058080934c277df9d47c124132
Ali Raza ❄ <32830427+alirazacodes@users.noreply.github.com>
2023-11-01 15:05:40 +0500

    readjusted searchbar

--- components/Header.js
+++ components/Header.js
@@ -2,23 +2,23 @@
 import Link from "next/link";
 import Headroom from "headroom.js";
 import { useEffect, useState } from "react";
-const IMG = "/logo2.png";
+const IMG = "/logo.png";
 import { parse } from "url";
 import { Input } from "@mui/material";
 
 export default () => {
   const [selected, setSelected] = useState(0);
+
   useEffect(() => {
     let headroom = new Headroom(document.getElementById("navbar-main"));
     // initialise
     headroom.init();
   });
+
   let currentUrl = /*window ? window.location.href : */ "/";
 
   useEffect(() => {
-    // currentUrl = window.location.href;
     const parsedUrl = parse(currentUrl, true);
-
     const urlPath = parsedUrl.path;
     console.log(urlPath);
 
@@ -30,32 +30,21 @@ export default () => {
       setSelected(2);
       return;
     }
-    // if (urlPath == "/Registration") {
-    //   setSelected(3);
-    //   return;
-    // }
     if (urlPath == "/Profile") {
-      //   document.getElementById("navbar-main").style.visibility = "hidden";
-
       setSelected(4);
       return;
     }
-    // document.getElementById("navbar-main").style.visibility = "visible";
-
     setSelected(0);
   }, [currentUrl]);
+
   return (
     <nav
       id="navbar-main"
-      className="navbar-transparent headroom head flex flex-row justify-between items-center"
+      className="navbar-transparent headroom head flex flex-row justify-between items-center px-8"
     >
-      <div className="flex font-bold flex-row items-center">
-        <div>
-          <Input
-            className="text-white w-80 m-2 p-2"
-            placeholder="Search accounts, 0x or name.Blockbadge..."
-          />
-        </div>
+      {/* Logo */}
+      <div className="flex items-center">
+        <img src={IMG} alt="Logo" className="h-10 w-auto mr-20" />
         <div className="flex font-bold flex-row items-center">
           <Link className="mr-4 p-3 " href="/Home/">
             {selected == 1 ? (
@@ -72,13 +61,7 @@ export default () => {
               <>Attestations</>
             )}
           </Link>
-          {/* <Link className="mr-4 p-3 " href="/Registration">
-            {selected == 3 ? (
-              <div className="border-b-2 text-blue-700">Register</div>
-            ) : (
-              <>Register</>
-            )}
-          </Link> */}
+
           <Link className="mr-4 p-3 " href="/Profile">
             {selected == 4 ? (
               <div className="border-b-2 text-blue-700">Profile</div>
@@ -88,7 +71,15 @@ export default () => {
           </Link>
         </div>
       </div>
-      <w3m-button />
+
+      {/* Search Bar and Button */}
+      <div className="flex items-center">
+        <Input
+          className="text-white w-80 mr-4 p-2"
+          placeholder="Search accounts, 0x or name.Blockbadge..."
+        />
+        <w3m-button />
+      </div>
     </nav>
   );
 };
