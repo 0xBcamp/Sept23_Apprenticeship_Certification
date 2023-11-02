c328fb765ca072316823f3e0aeb1a75c6a6eb7a3
Theresa Whynot <132946455+theresa-whynot@users.noreply.github.com>
2023-11-02 13:55:43 -0400

    Update SingleCertificationCard.js with LinkedIn Button

--- components/ProfilePart/SingleCertificationCard.js
+++ components/ProfilePart/SingleCertificationCard.js
@@ -18,41 +18,60 @@ export default ({ item }) => {
       console.error("Error parsing JSON:", error);
     }
   }, [decodedDataJson]);
-  const logo = "/logo2.png";
-  return (
-    <>
-      <Fade bottom duration={2000}>
-        <div className="card card-body flex  text-white">
-          <div className="flex text-center justify-between space-x-2">
-            {/* <div>
-            <ENSAvatar address={attester} size={50} />
-          </div> */}
-            <div>
-              <img src={logo} alt="" className="w-14 h-14" />
-            </div>
-            <div>
-              <EASSlicedAddress Address={attester} />
-              <EASDate date={timeCreated} />
-            </div>
-            <div>
-              <p>Attested</p>
-              <p>
-                {decodedDataJsonArr.map((item, i) => {
-                  return (
-                    <EASMessage
-                      key={i}
-                      Message={
-                        item.value.name.toLowerCase() == "message" &&
-                        item.value.value
-                      }
-                    />
-                  );
-                })}
-              </p>
-            </div>
-          </div>
-        </div>
-      </Fade>
-    </>
-  );
-};
+
+
+
+    const logo = "/logo2.png";
+
+    const organizationName = 'orgname'; // Replace with the actual organization name the user entered
+
+    const easAttestationUrl = 'easattestationurl'; // Replace with the actual EAS attestation URL that is output upon transaction completion
+
+    const transactionHash = 'transactionhash'; // Replace with the actual transaction hash that is output upon transaction completion
+
+    const handleCertificationClick = () => {
+        const linkedinUrl = `https://www.linkedin.com/profile/add?startTask=CERTIFICATION_NAME&name=${organizationName}&organizationName=${organizationName}&certUrl=${easAttestationUrl}&certId=${transactionHash}`;
+        window.open(linkedinUrl, '_blank');
+    };
+
+    return (
+        <>
+            <Fade bottom duration={2000}>
+                <div className="flex" style={{ width: '100%' }}>
+                    <div className="card card-body flex text-white" style={{ width: '75%' }}>
+                        <div className="flex text-center justify-between space-x-2">
+                            <div>
+                                <img src={logo} alt="" className="w-14 h-14" />
+                            </div>
+                            <div>
+                                <EASSlicedAddress Address={attester} />
+                                <EASDate date={timeCreated} />
+                            </div>
+                            <div>
+                                <p>Attested</p>
+                                <p>
+                                    {decodedDataJsonArr.map((item, i) => {
+                                        return (
+                                            <EASMessage
+                                                key={i}
+                                                Message={
+                                                    item.value.name.toLowerCase() == 'message' &&
+                                                    item.value.value
+                                                }
+                                            />
+                                        );
+                                    })}
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+                    <div style={{ width: '25%', marginLeft:'10px'}} className="card card-body flex text-white">
+                        <button onClick={handleCertificationClick}>
+                            Add Certification to My LinkedIn
+                        </button>
+                    </div>
+                </div>
+            </Fade>
+        </>
+    );
+}
